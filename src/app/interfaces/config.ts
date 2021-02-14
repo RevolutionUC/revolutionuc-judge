e333c8a645c43f33834d1f026bf5e5738858a870
WillCPP <WillCPP@users.noreply.github.com>
2021-02-13 22:04:46 -0500

    Create config.ts

---
+++ src/app/interfaces/config.ts
@@ -0,0 +1,14 @@
+export interface ConfigDto {
+    year: number
+    generalGroupCount: number
+    generalJudgesPerGroup: number
+    generalGroupsPerProject: number
+    titleColumn: string
+    urlColumn: string
+    taglineColumn: string
+    descriptionColumn: string
+    submitterEmailColumn: string
+    submitterNameColumn: string
+    categoryColumn: string
+    tableNumberColumn: string
+  }
