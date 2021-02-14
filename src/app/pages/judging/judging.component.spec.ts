0e6146ce3aa5a069344f34e6a31a7341516b6008
WillCPP <WillCPP@users.noreply.github.com>
2021-02-13 21:51:29 -0500

    Add judging component
    This is the component judges will interact with

---
+++ src/app/pages/judging/judging.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JudgingComponent } from './judging.component';
+
+describe('JudgingComponent', () => {
+  let component: JudgingComponent;
+  let fixture: ComponentFixture<JudgingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ JudgingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JudgingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/judging/judging.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-judging',
+  templateUrl: './judging.component.html',
+  styleUrls: ['./judging.component.css']
+})
+export class JudgingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
