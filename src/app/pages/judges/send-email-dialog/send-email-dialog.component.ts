8ff237da9c9a4416c89f02ff39dab375e391a34a
Dev Agrawal <devagrawal09@gmail.com>
2021-02-26 14:44:04 -0500

    changed info email to post event email

--- src/app/pages/judges/send-email-dialog/send-email-dialog.component.ts
+++ src/app/pages/judges/send-email-dialog/send-email-dialog.component.ts
@@ -23,7 +23,7 @@ export class SendEmailDialogComponent {
 
   sendEmail() {
     const payload: SendEmailDto = {
-      template: `infoEmailJudges`,
+      template: `postEmailJudgeEmail`,
       recipent: this.judge.email,
       dryRun: this.dryRun.value
     };
